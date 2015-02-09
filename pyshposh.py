1d60f4b7314404dd73e9ec05ffc4ce81ec4b00d7
Scott Pack <scott.pack@gmail.com>
2015-02-09 00:56:51 -0500

    Adding some exception handling around file operations.

--- pyshposh.py
+++ pyshposh.py
@@ -1,4 +1,3 @@
-▽
 #!/usr/bin/python
 
 import winrm, sys, argparse, yaml
@@ -6,17 +5,35 @@ import winrm, sys, argparse, yaml
 inputfile = ''
 
 parser = argparse.ArgumentParser(description='Short Sample PyshPosh App')
+parser.add_argument('-s', '--settings', action='store', dest='settingsfile', default='settings.yaml', help='Path to settings yaml file')
 
-parser.add_argument('-i', action='store', dest='inputfile', help='Filename of Powershell Script to Run')
-parser.add_argument('-s', action='store', dest='settingsfile', default='settings.yaml', help='Path to settings yaml file')
+requiredNamed = parser.add_argument_group('required named arguments')
+parser.add_argument('-i', '--script', action='store', dest='inputfile', required = True, help='Filename of Powershell Script to Run')
 
 results = parser.parse_args()
 
-with open (results.inputfile, "r") as myfile:
-  ps_script=myfile.read()
+# Read in the settings
+try:
+  file = open('settings.yml')
+  settings = yaml.load(file)
+except IOError as e:
+  print "I/O error({0}): {1}: {2}".format(e.errno, e.strerror, results.inputfile)
+  exit(0)
+except:
+  print "Unexpected error:", sys.exc_info()[0]
+  exit(0)
 
-file = open('settings.yml')
-settings = yaml.load(file)
+# Now read in the script
+ps_script = ''
+try:
+  myfile = open(results.inputfile, "r")
+  ps_script = myfile.read()
+except IOError as e:
+  print "I/O error({0}): {1}: {2}".format(e.errno, e.strerror, results.inputfile)
+  exit(0)
+except:
+  print "Unexpected error:", sys.exc_info()[0]
+  exit(0)
 
 for host in settings['hosts'] :
 
